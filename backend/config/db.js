177ccf986dab06f8c5a16e8ff86f57b88a6e91b8
Anu8084 <anuragbabaojha@gmail.com>
2025-05-01 23:34:52 +0530

    changes done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,3 @@
-// const mongoose = require('mongoose');
-// require('dotenv').config(); // Load environment variables
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('✅ MongoDB Connected Successfully');
-//   } catch (error) {
-//     console.error('❌ MongoDB Connection Error:', error);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
 
 
 const mongoose = require('mongoose');
