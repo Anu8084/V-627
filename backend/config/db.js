a5d67530f33365fc6cad0697284bed38d4e76cac
Anu8084 <anuragbabaojha@gmail.com>
2025-05-01 23:31:20 +0530

    changes in db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,28 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config(); // Load environment variables
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('✅ MongoDB Connected Successfully');
+//   } catch (error) {
+//     console.error('❌ MongoDB Connection Error:', error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB Connected Successfully');
   } catch (error) {
     console.error('❌ MongoDB Connection Error:', error);
